7be7b28e24ea56df596f72edf23b641ee2b5b2b8
mth2610 <mth2610@gmail.com>
2016-07-07 13:59:18 +0700

    update

--- envihotspot/envihotspot/settings.py
+++ envihotspot/envihotspot/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = '/'
+
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#SECURE_SSL_REDIRECT = True
+#SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
 
 # Application definition
 
@@ -37,6 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_gis',
+    'sslserver',
+    'oauth2_provider',
+    'social.apps.django_app.default',
+    'rest_framework_social_oauth2',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +67,7 @@ ROOT_URLCONF = 'envihotspot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,10 +75,21 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social.apps.django_app.context_processors.backends',
+                'social.apps.django_app.context_processors.login_redirect',
             ],
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # OAuth
+        'rest_framework.parsers.JSONParser',
+        'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+        'rest_framework_social_oauth2.authentication.SocialAuthentication',
+    ]
+}
 
 WSGI_APPLICATION = 'envihotspot.wsgi.application'
 
@@ -101,6 +124,26 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTHENTICATION_BACKENDS = [
+
+    # Facebook OAuth2
+    'social.backends.facebook.FacebookAppOAuth2',
+    'social.backends.facebook.FacebookOAuth2',
+
+    # django-rest-framework-social-oauth2
+    'rest_framework_social_oauth2.backends.DjangoOAuth2',
+
+    # Django
+    'django.contrib.auth.backends.ModelBackend',
+
+]
+# Facebook configuration
+SOCIAL_AUTH_FACEBOOK_KEY = '247490098968695'
+SOCIAL_AUTH_FACEBOOK_SECRET = '3c266e26f0348976711fbedfe2399633'
+
+# Define SOCIAL_AUTH_FACEBOOK_SCOPE to get extra permissions from facebook. Email is not sent by default, to get it, you must request the email permission:
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
@@ -119,3 +162,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'collectedstatic/assets')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- envihotspot/envihotspot/urls.py
+++ envihotspot/envihotspot/urls.py
@@ -13,9 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from home import views as home_views
+from mappage import views as mappage_views
+from rest_api import views as api_views
+from rest_framework import routers
+from account import views as account_views
+
+router = routers.DefaultRouter()
+router.register(r'hotspots', api_views.HotSpotViewSet, base_name='hotspots')
 
 urlpatterns = [
+    url('^', include('django.contrib.auth.urls')),
+    url(r'^api/', include(router.urls)),
+    url(r'^auth/', include('rest_framework_social_oauth2.urls')),
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', home_views.home),
+    url(r'signup/', account_views.signup),
+    url(r'signup_success/', account_views.signup_success),
+    url(r'^mappage/', include('mappage.urls')),
+    url(r'^hotspots/', include('hotspots.urls')),
+    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset')
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
